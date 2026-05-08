a05ac6002c47f91eae28f8a3ee03f81ee20fb4d7
Diego <diego.spv7@gmail.com>
2026-05-08 19:26:42 -0300

    Inicializando o repositório do projeto de mobile

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: false
+})
+export class HomePage {
+
+  constructor(private router: Router) {}
+
+  irParaLogin() {
+    this.router.navigate(['login']);
+  }
+
+}
